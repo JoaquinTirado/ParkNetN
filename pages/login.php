0774663023bb34d81cafeb44feb7c34ffa163d8e
Alan <alan@parknet.pr>
2018-06-14 16:05:48 -0400

    with TestDaLogin title

--- pages/login.php
+++ pages/login.php
@@ -17,7 +17,7 @@ License: You must have a valid license purchased only from themeforest(the above
 	<head>
 		<meta charset="utf-8" />
 		<title>
-			Parknet | Login
+			TestDaLogin
 		</title>
 		<meta name="description" content="Latest updates and statistic charts">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
